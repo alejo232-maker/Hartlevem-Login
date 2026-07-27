fb46ccee3e847000d114d2abf3198f42236dcccf
alejo232-maker <yulianis1232@gmail.com>
2026-07-26 20:03:40 -0500

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+
+echo "===================================="
+echo "      HARTLEVEM LOGIN v2.0"
+echo "===================================="
+echo
